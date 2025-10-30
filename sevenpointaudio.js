2d5256c2c526e0e3568e1f841e6da34a92d4f435
Jeff Ball <jeffball@7pointaudio.com>
2025-10-30 14:29:45 -0400

    Ran yarn

--- sevenpointaudio.js
+++ sevenpointaudio.js
@@ -12,69 +12,68 @@ class SevenPointAudioModuleInstance extends InstanceBase {
 		super(internal)
 	}
 
-   //Required method for module to operate
-   getConfigFields() {
+	//Required method for module to operate
+	getConfigFields() {
 		return UpdateConfigFields.getConfigFields()
 	}
 
-   //Init the module
-   async init(config) {
-      this.config = config
-      //Create the UDP object before loading the module
-      this.initUDP()   
-      this.updateStatus(InstanceStatus.Ok)
-      this.updateActions()
-      this.updateVariableDefinitions()
-   }
+	//Init the module
+	async init(config) {
+		this.config = config
+		//Create the UDP object before loading the module
+		this.initUDP()
+		this.updateStatus(InstanceStatus.Ok)
+		this.updateActions()
+		this.updateVariableDefinitions()
+	}
 
-   initUDP() {
-      //Destroy any pre-existing object
-      if (this.udp) {
-         this.log('info', 'Destroying existing UDP connection')
-         this.udp.destroy()
-         delete this.udp
-      }
+	initUDP() {
+		//Destroy any pre-existing object
+		if (this.udp) {
+			this.log('info', 'Destroying existing UDP connection')
+			this.udp.destroy()
+			delete this.udp
+		}
 
-      //Get the host/port settings from the config, if present
-      const host = this.config.host || DEFAULT_HOST
-      const port = this.config.port || DEFAULT_PORT
+		//Get the host/port settings from the config, if present
+		const host = this.config.host || DEFAULT_HOST
+		const port = this.config.port || DEFAULT_PORT
 
-      this.log('info', `Creating new UDPHelper - Host: ${host}, Port: ${port}`)
+		this.log('info', `Creating new UDPHelper - Host: ${host}, Port: ${port}`)
 
-      //Create the UDPHelper object
-      try {
-         this.udp = new UDPHelper(host, port)
+		//Create the UDPHelper object
+		try {
+			this.udp = new UDPHelper(host, port)
 
-         //Capture the state_change event
-         this.udp.on('status_change', (status, message) => {
-            this.updateStatus(status, message)
-         })
+			//Capture the state_change event
+			this.udp.on('status_change', (status, message) => {
+				this.updateStatus(status, message)
+			})
 
-         //Capture the error even
-         this.udp.on('error', (err) => {
-            this.log('error', 'UDP error: ' + err.message)
-            this.updateStatus(InstanceStatus.ConnectionFailure, err.message)
-         })
-      } catch (error) {
-         this.log('error', `Failed to create UDP helper: ${error.message}`)
-      }
-
-   }
+			//Capture the error even
+			this.udp.on('error', (err) => {
+				this.log('error', 'UDP error: ' + err.message)
+				this.updateStatus(InstanceStatus.ConnectionFailure, err.message)
+			})
+		} catch (error) {
+			this.log('error', `Failed to create UDP helper: ${error.message}`)
+		}
+	}
 
 	// When module gets deleted
 	async destroy() {
-      //Close any open UDPHelper objects
-      if (this.udp) {
-         this.udp.destroy()
-      }
-      delete this.udp
+		//Close any open UDPHelper objects
+		if (this.udp) {
+			this.udp.destroy()
+		}
+		delete this.udp
 
 		this.log('info', 'Destroying Seven Point Audio Module')
 	}
 
 	async configUpdated(config) {
 		this.config = config
-      this.initUDP()
+		this.initUDP()
 	}
 
 	updateActions() {
--- src/actions.js
+++ src/actions.js
@@ -1,196 +1,195 @@
 module.exports = function (self) {
-   //Basic UDP send message
-   //NOTE: The UDP message does **NOT** end in a CR/LF
-   const sendUDPMessage = (msg, msgId = "Sending UDP Message") => {
-      if (msg === undefined)
-      {
-         self.log('warn', 'Message is undefined')
-         return
-      }
+	//Basic UDP send message
+	//NOTE: The UDP message does **NOT** end in a CR/LF
+	const sendUDPMessage = (msg, msgId = 'Sending UDP Message') => {
+		if (msg === undefined) {
+			self.log('warn', 'Message is undefined')
+			return
+		}
 
-      if (self.udp) {
-         //No matter what I try the debug message is never logged; change to info to see messages
-         //Of the opinion this message is too verbose for info level logging
-         self.log('debug', `${msgId}: ${msg}`)
-         self.udp.send(msg)
-      } else {
-         self.log('warn', 'UDP not initialized')
-      }
-   }
+		if (self.udp) {
+			//No matter what I try the debug message is never logged; change to info to see messages
+			//Of the opinion this message is too verbose for info level logging
+			self.log('debug', `${msgId}: ${msg}`)
+			self.udp.send(msg)
+		} else {
+			self.log('warn', 'UDP not initialized')
+		}
+	}
 
-   //Create a Toggle Action that is repeatable
-   const createToggleAction = (name, cmdName = "") => {
-      if (cmdName == "") {
-         cmdName = name.toLowerCase()
-      } else {
-         cmdName = cmdName.toLowerCase()
-      }
+	//Create a Toggle Action that is repeatable
+	const createToggleAction = (name, cmdName = '') => {
+		if (cmdName == '') {
+			cmdName = name.toLowerCase()
+		} else {
+			cmdName = cmdName.toLowerCase()
+		}
 
-      const stateId = cmdName + 'State'
+		const stateId = cmdName + 'State'
 
-      return {
-         name: name,
-         options: [
-            {
-               type: 'dropdown',
-               label: `${name} Control`,
-               id: stateId,
-               default: 'toggle',
-               choices: [
-                  { id: 'toggle', label: `Toggle ${name}` },
-                  { id: 'on', label: `${name} On` },
-                  { id: 'off', label: `${name} Off` }
-               ]
-            }
-         ],
-         callback: async (event, context) => {
-            let opt = event.options
-            let cmd = cmdName
+		return {
+			name: name,
+			options: [
+				{
+					type: 'dropdown',
+					label: `${name} Control`,
+					id: stateId,
+					default: 'toggle',
+					choices: [
+						{ id: 'toggle', label: `Toggle ${name}` },
+						{ id: 'on', label: `${name} On` },
+						{ id: 'off', label: `${name} Off` },
+					],
+				},
+			],
+			callback: async (event, context) => {
+				let opt = event.options
+				let cmd = cmdName
 
-            switch (opt[stateId]) {
-               case 'on':
-                  cmd += ' 1'
-                  break
-               case 'off':
-                  cmd += ' 0'
-                  break
-            }
+				switch (opt[stateId]) {
+					case 'on':
+						cmd += ' 1'
+						break
+					case 'off':
+						cmd += ' 0'
+						break
+				}
 
-            sendUDPMessage(cmd, `${name} Command`)
-         }
-      }
-   }
+				sendUDPMessage(cmd, `${name} Command`)
+			},
+		}
+	}
 
 	self.setActionDefinitions({
-      announcement:  createToggleAction('Announcement'),
-      compressor:    createToggleAction('Compressor'),
-      crossfade:     createToggleAction('Crossfade'),
-      defaultfade:   createToggleAction('Default Fade', 'defaultfade'),
-      equalizer:     createToggleAction('Equalizer'),
-      loop:          createToggleAction('Loop'),
-      pause: {
-         name: 'Pause',
-         options: [],
-         callback: async (event) => {
-            let cmd = 'pause'
-            sendUDPMessage(cmd, 'Pause Command')
-         }
-      },
-      play: {
-         name: "Play",
-         options: [],
-         callback: async (event) => {
-            let cmd = 'play'
-            sendUDPMessage(cmd, 'Play Command')
-         }
-      },
-      playbutton: {
-         name: 'Play Button',
-         options: [
-            {
-               type: 'textinput',
-               label: 'Button ID',
-               id: 'buttonid',
-            },
-            {
-               type: 'checkbox',
-               label: 'Sound Over Sound',
-               id: 'sos',
-               default: false
-            }
-         ],
-         callback: async (event, context) => {
-            let opt = event.options
-            let buttonId = await context.parseVariablesInString(opt.buttonid)
-            let sos = opt.sos ? 1 : 0
-            let cmd = `playbutton ${buttonId} ${sos}`
+		announcement: createToggleAction('Announcement'),
+		compressor: createToggleAction('Compressor'),
+		crossfade: createToggleAction('Crossfade'),
+		defaultfade: createToggleAction('Default Fade', 'defaultfade'),
+		equalizer: createToggleAction('Equalizer'),
+		loop: createToggleAction('Loop'),
+		pause: {
+			name: 'Pause',
+			options: [],
+			callback: async (event) => {
+				let cmd = 'pause'
+				sendUDPMessage(cmd, 'Pause Command')
+			},
+		},
+		play: {
+			name: 'Play',
+			options: [],
+			callback: async (event) => {
+				let cmd = 'play'
+				sendUDPMessage(cmd, 'Play Command')
+			},
+		},
+		playbutton: {
+			name: 'Play Button',
+			options: [
+				{
+					type: 'textinput',
+					label: 'Button ID',
+					id: 'buttonid',
+				},
+				{
+					type: 'checkbox',
+					label: 'Sound Over Sound',
+					id: 'sos',
+					default: false,
+				},
+			],
+			callback: async (event, context) => {
+				let opt = event.options
+				let buttonId = await context.parseVariablesInString(opt.buttonid)
+				let sos = opt.sos ? 1 : 0
+				let cmd = `playbutton ${buttonId} ${sos}`
 
-            sendUDPMessage(cmd, `Play Button ${buttonId} Command`)
-         }
-      },
-      rewind: {
-         name: 'Rewind',
-         options: [],
-         callback: async (event) => {
-            let cmd = 'rewind'
-            sendUDPMessage(cmd, 'Rewind Command')
-         }
-      },
-      stop: {
-         name: 'Stop',
-         options: [],
-         callback: async (event) => {
-            let cmd = 'stop'
-            sendUDPMessage(cmd, 'Stop Command')
-         }
-      },
-      stopall: {
-        name: 'Stop All',
-        options: [],
-        callback: async (event) => {
-            let cmd = 'stopall'
-            sendUDPMessage(cmd, 'Stop All Command')
-        } 
-      },
-      volume: {
-         name: 'Volume',
-         options: [
-            {
-               type: 'number',
-               label: 'Volume (0-100)',
-               id: 'volumelvl',
-               default: 50,
-               min: 0,
-               max: 100
-            }
-         ],
-         callback: async (event, context) => {
-            let opt = event.options
-            let vol = opt.volumelvl
-            let cmd = `volume ${vol}`
+				sendUDPMessage(cmd, `Play Button ${buttonId} Command`)
+			},
+		},
+		rewind: {
+			name: 'Rewind',
+			options: [],
+			callback: async (event) => {
+				let cmd = 'rewind'
+				sendUDPMessage(cmd, 'Rewind Command')
+			},
+		},
+		stop: {
+			name: 'Stop',
+			options: [],
+			callback: async (event) => {
+				let cmd = 'stop'
+				sendUDPMessage(cmd, 'Stop Command')
+			},
+		},
+		stopall: {
+			name: 'Stop All',
+			options: [],
+			callback: async (event) => {
+				let cmd = 'stopall'
+				sendUDPMessage(cmd, 'Stop All Command')
+			},
+		},
+		volume: {
+			name: 'Volume',
+			options: [
+				{
+					type: 'number',
+					label: 'Volume (0-100)',
+					id: 'volumelvl',
+					default: 50,
+					min: 0,
+					max: 100,
+				},
+			],
+			callback: async (event, context) => {
+				let opt = event.options
+				let vol = opt.volumelvl
+				let cmd = `volume ${vol}`
 
-            sendUDPMessage(cmd, `Volume ${vol} Command`)
-         }
-      },
-      volumedown: {
-         name: 'Volume Down',
-         options: [
-            {
-               type: 'number',
-               label: 'Volume (0-10)',
-               id: 'volumelvl',
-               default: 5,
-               min: 0,
-               max: 10
-            }
-         ],
-         callback: async (event, context) => {
-            let opt = event.options
-            let vol = opt.volumelvl
-            let cmd = `volumedown ${vol}`
+				sendUDPMessage(cmd, `Volume ${vol} Command`)
+			},
+		},
+		volumedown: {
+			name: 'Volume Down',
+			options: [
+				{
+					type: 'number',
+					label: 'Volume (0-10)',
+					id: 'volumelvl',
+					default: 5,
+					min: 0,
+					max: 10,
+				},
+			],
+			callback: async (event, context) => {
+				let opt = event.options
+				let vol = opt.volumelvl
+				let cmd = `volumedown ${vol}`
 
-            sendUDPMessage(cmd, `Volume Down ${vol} Command`)
-         }
-      },
-      volumeup: {
-         name: 'Volume Up',
-         options: [
-            {
-               type: 'number',
-               label: 'Volume (0-10)',
-               id: 'volumelvl',
-               default: 5,
-               min: 0,
-               max: 10
-            }
-         ],
-         callback: async (event, context) => {
-            let opt = event.options
-            let vol = opt.volumelvl
-            let cmd = `volumeup ${vol}`
+				sendUDPMessage(cmd, `Volume Down ${vol} Command`)
+			},
+		},
+		volumeup: {
+			name: 'Volume Up',
+			options: [
+				{
+					type: 'number',
+					label: 'Volume (0-10)',
+					id: 'volumelvl',
+					default: 5,
+					min: 0,
+					max: 10,
+				},
+			],
+			callback: async (event, context) => {
+				let opt = event.options
+				let vol = opt.volumelvl
+				let cmd = `volumeup ${vol}`
 
-            sendUDPMessage(cmd, `Volume Up ${vol} Command`)
-         }
-      },
+				sendUDPMessage(cmd, `Volume Up ${vol} Command`)
+			},
+		},
 	})
 }
